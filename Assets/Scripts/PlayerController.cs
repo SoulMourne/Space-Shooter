b96238e985e950fd6a5e628e98ffb897bafa813f
SoulMourne <goodwinjeanpaul@yahoo.fr>
2016-02-20 12:19:04 +0100

    Ajout du mouvement du vaisseau, sa rotation, et l'empeche de sortir de l'aire de jeu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); //Get the horizontal axis of the player
+        float moveVertical = Input.GetAxis("Vertical"); //Get the vertical axis of the player
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Rigidbody rigidbody = GetComponent<Rigidbody>(); //Get the rigidbody
+
+        rigidbody.velocity = movement * speed;
+        rigidbody.position = new Vector3(Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
+        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+    }
+}
