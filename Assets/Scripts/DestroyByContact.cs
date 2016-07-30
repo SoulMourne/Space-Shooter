c634cd2fc37bc8d37df5e2483c69e1a2dc1752f3
SoulMourne <goodwinjeanpaul@yahoo.fr>
2016-07-30 17:43:09 +0200

    Added Score

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,22 @@ public class DestroyByContact : MonoBehaviour
     public GameObject explosion;
     public GameObject playerExplosion;
 
+    private GameController gameController;
+    public int scoreValue;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Canno find 'Game Controller' script");
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Boundary")
@@ -13,6 +29,7 @@ public class DestroyByContact : MonoBehaviour
             Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
             if (other.tag == "Player")
                 Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.AddScore(scoreValue);
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
